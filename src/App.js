f20f2baddac0214ae1cc7d95e92db3267c4036b3
pavithrancse21@gmail.com <pavithrancse21@gmail.com>
2023-06-19 16:32:21 +0530

    with '#' will be ignored, and an empty message aborts the commit.

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+// components
+import Banner from './components/Banner';
+import Header from './components/Header';
+import Nav from './components/Nav';
+import About from './components/About';
+import Services from './components/Services';
+
+const App = () => {
+  return (
+    <div className='bg-site bg-no-repeat bg-cover overflow-hidden'>
+      <Header />
+      <Banner />
+      <Nav />
+      <About />
+      <Services />
+      <div className='h-[100px]'></div>
+    </div>
+  );
+};
+
+export default App;
